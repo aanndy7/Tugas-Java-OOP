21f3ce9e04b82f1288d9b1c31814eac1f08ead7e
Andi Rustandi <32457564+aanndy7@users.noreply.github.com>
2017-11-06 21:30:50 +0700

    Add files via upload

---
+++ A.java
@@ -0,0 +1,9 @@
+public class A {
+	public static void main (String args[] ) {
+		int a = 20, b = 10;
+		calculatormain cal = new calculatormain();
+		
+		cal.pertambahan(a, b);
+		cal.pengurangan(a, b);
+	}
+}
---
+++ VariabelStatic.java
@@ -0,0 +1,12 @@
+public class VariabelStatic {
+	
+	public static String nama;
+		private String namaPrivate;
+		public static void main (String[] args) {
+			System.out.println()
+				VariabelStatic var = new VariabelStatic();
+				var.namaPrivate = "Andi Rustandi";
+				VariabelStatic.nama = " Andi Rustandi";
+
+	}	
+}
